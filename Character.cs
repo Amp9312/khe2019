1881f1ba34e87cea154716b5dc8cbe71e6c3714a
Anthony Hake <lvlup97@gmail.com>
2019-09-28 11:16:10 -0400

    New Documentation
    Cleaned up character class with more precise documentation

--- Character.cs
+++ Character.cs
@@ -1,58 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// This is an abstract class that all characters needs to inherit from
-/// </summary>
+// <summary>
+// This is an abstract class that all characters needs to inherit from
+// </summary>
 public abstract class Character : MonoBehaviour
 {
-
-    /// <summary>
-    /// The Player's movement speed
-    /// </summary>
     [SerializeField]
     private float speed;
 
     private Animator animator;
 
-    /// <summary>
-    /// The Player's direction
-    /// </summary>
+    //The Player's direction
     protected Vector2 direction;
 
     // Use this for initialization
-    void Start()
+    protected virtual void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    /// <summary>
-    /// Update is marked as virtual, so that we can override it in the subclasses
-    /// </summary>
+    // Update is marked as virtual, so that we can override it in the subclasses
     protected virtual void Update()
     {
         Move();
     }
 
-    /// <summary>
-    /// Moves the player
-    /// </summary>
+    // Moves the player
     public void Move()
     {
         //Makes sure that the player moves
         transform.Translate(direction * speed * Time.deltaTime);
 
-        //Animate's the Player's movement
-        AnimateMovement(direction);
+        if(direction.x != 0 || direction.y != 0)
+        {
+            //Animate's the Player's movement
+            AnimateMovement(direction);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+
+
     }
 
-    /// <summary>
-    /// Makes the player animate in the correct direction
-    /// </summary>
+    // Makes the player animate in the correct direction
     /// <param name="direction"></param>
     public void AnimateMovement(Vector2 direction)
     {
+        animator.SetLayerWeight(1, 1);
         //Sets the animation parameter so that he faces the correct direction
         animator.SetFloat("X", direction.x);
         animator.SetFloat("Y", direction.y);
