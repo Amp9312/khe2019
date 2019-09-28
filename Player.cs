3d5aced9b494f8dfc237c1f55847ce3b37885fd9
Anthony Hake <lvlup97@gmail.com>
2019-09-28 11:17:14 -0400

    Added functionality for changing health/mana bar

--- Player.cs
+++ Player.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// This is the player script, it contains functionality that is specific to the Player
-/// </summary>
+// This is the player script, it contains functionality that is specific to the Player
+
 public class Player : Character
 {
+    [SerializeField]
+    private Stat health;
+    [SerializeField]
+    private Stat mana;
+  
+    private float initHealth = 100;
+    private float initMana = 50;
+    // overriding the characters update function, so that we can execute our own functions
+    protected override void Start()
+    {
+        health.Initialize(initHealth, initHealth);
+        mana.Initialize(initMana, initMana);
 
-    /// <summary>
-    /// We are overriding the characters update function, so that we can execute our own functions
-    /// </summary>
+        base.Start();
+    }
     protected override void Update()
     {
         //Executes the GetInput function
@@ -19,12 +29,26 @@ public class Player : Character
         base.Update();
     }
 
-    /// <summary>
-    /// Listen's to the players input
-    /// </summary>
+
+
+    // Listen's to the players input
     private void GetInput()
     {
         direction = Vector2.zero;
+
+        /*health bar debugging
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            health.MyCurrentValue -= 10;
+            mana.MyCurrentValue -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            health.MyCurrentValue += 10;
+            mana.MyCurrentValue += 10;
+        }
+        end of health bar debugging */
+
 
         if (Input.GetKey(KeyCode.W)) //Moves up
         {
@@ -32,9 +56,9 @@ public class Player : Character
         }
         if (Input.GetKey(KeyCode.A)) //Moves left
         {
-            direction += Vector2.left; //Moves down
+            direction += Vector2.left; 
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)) //moves down
         {
             direction += Vector2.down;
         }
